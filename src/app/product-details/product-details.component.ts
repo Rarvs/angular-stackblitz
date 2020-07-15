7a9b45ccadb29b50ff4282a506d19654ea0398e9
Rarvs <rafael.rarvs@gmail.com>
2020-07-15 10:51:39 -0300

    4 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -10,12 +10,10 @@ import { products } from '../products';
 export class ProductDetailsComponent implements OnInit {
   product;
 
-  constructor(
-    private route: ActivatedRoute,
-  ) { }
+  constructor(private route: ActivatedRoute) { }
     
   ngOnInit() {
-    this.route.paramMap.subscribe(params=>{
+    this.route.paramMap.subscribe(params => {
       this.product = products[+params.get('productId')];
     });
   }
